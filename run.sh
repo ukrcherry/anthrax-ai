6010d7a20d1fa2afa7d44d5e86213e6e3485881a
ukrcherry <aleksi.eesti@gmail.com>
2025-04-20 20:42:36 +0300

    fixing Core::Audio::Init error on Ubuntu

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 # !/bin/bash
 
 cd assets
-../build/demos/demo-models/anthraxAI-demo-models
+
+# Ubuntu 24.04.02
+# attempts to launch `../build/demos/demo-models/anthraxAI-demo-models`
+# would return an `Core::Audio::Init(): Device was not created` error continually
+# a temporary fix:
+# forcing loading the system `openal` instead of the one built by anthrax
+# `build/engine/libs/openal-soft/openal-soft/libopenal.so.1`
+# `apt install libopenal1` if not installed
+
+LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libopenal.so.1 ../build/demos/demo-models/anthraxAI-demo-models
